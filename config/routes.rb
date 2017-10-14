d590b2d6cc7d4239fbfd48cc285f1022be9eb2c2
EnziinAcademy <admin@enziin.com>
2017-10-14 04:25:11 +0000

    Add models Category - Product - User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :users
+
+  resources :categories do
+    resources :products
+  end
+
+  resources :products
+  
   get 'pages/home'
 
   get 'pages/about'
